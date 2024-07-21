ae73a57817e2be3d2239d55433f41b0b5633d280
lochinbek <lochinbekk77@gmail.com>
2024-07-22 01:53:18 +0500

    tugadi

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,6 +15,11 @@ function Login() {
     let [hodimlar, setHodimlar] = useState('')
     let [izoh, setIzoh] = useState('')
     let [imageUrl, setImageUrl] = useState(null)
+    let [dunyo,setDunyo]=useState('')
+    let [instagram,setInstagram]=useState('')
+    let [telegram,setTelegram]=useState('')
+    let [feyzbuk,setFeyzbuk]=useState('')
+    let [github,setGithub]=useState('')
 
     let handleImageUpload = (event) => {
         const file = event.target.files[0];
@@ -41,20 +46,34 @@ function Login() {
             feyzbuk,
             github,
         }
+        localStorage.setItem('data', data);
         console.log(data)
+        setName('');
+        setEmail('');
+        setTel('');
+        setDavlat('');
+        setShahar('');
+        setYashashJoy('');
+        setHodimlar('');
+        setIzoh('');
+        setImageUrl(null);
+        setDunyo('');
+        setInstagram('');
+        setTelegram('');
+        setFeyzbuk('');
+        setGithub('');
     }
-    let [dunyo,setDunyo]=useState(false)
-    let [instagram,setInstagram]=useState(false)
-    let [telegram,setTelegram]=useState(false)
-    let [feyzbuk,setFeyzbuk]=useState(false)
-    let [github,setGithub]=useState(false)
+    let [dunyo1,setDunyo1]=useState(false)
+    let [instagram1,setInstagram1]=useState(false)
+    let [telegram1,setTelegram1]=useState(false)
+    let [feyzbuk1,setFeyzbuk1]=useState(false)
+    let [github1,setGithub1]=useState(false)
     function korinmas (e){
-        setDunyo(false)
-        setInstagram(false)
-        setTelegram(false)
-        setFeyzbuk(false)
-        setGithub(false)
-        console.log(e)
+        setDunyo1(false)
+        setInstagram1(false)
+        setTelegram1(false)
+        setFeyzbuk1(false)
+        setGithub1(false)
     }
     return (
         <div className='div-2'>
@@ -77,33 +96,33 @@ function Login() {
                     </div>
                     <div className='div-6'>
                         <label className='label' htmlFor="name1">Kompaniya nomi *</label>
-                        <input onChange={(e) => { setName(e.target.value) }} id='name1' type="text" placeholder="Kompaniya nomi" className="input input-bordered w-full " />
+                        <input value={name} onChange={(e) => { setName(e.target.value) }} id='name1' type="text" placeholder="Kompaniya nomi" className="input input-bordered w-full " />
                         <label className='label' htmlFor="name2">Email *</label>
-                        <input onChange={(e) => { setEmail(e.target.value) }} id='name2' type="text" placeholder="Email" className="input input-bordered w-full " />
+                        <input value={email} onChange={(e) => { setEmail(e.target.value) }} id='name2' type="text" placeholder="Email" className="input input-bordered w-full " />
                         <label className='label' htmlFor="name3">Telefon raqami *</label>
-                        <input onChange={(e) => { setTel(e.target.value) }} id='name3' type="text" placeholder="UZ +9989" className="input input-bordered w-full" />
+                        <input value={tel} onChange={(e) => { setTel(e.target.value) }} id='name3' type="text" placeholder="UZ +9989" className="input input-bordered w-full" />
                     </div>
                     <p className='label'>Linklar *</p>
                     <div className='div-10'>
                     <div className='div-7'>
-                        <button onClick={(e)=>{korinmas(e), setDunyo(true)}}><img src={img5} alt="" /></button>
-                        <button onClick={(e)=>{korinmas(e), setInstagram(true)}}><img src={img2} alt="" /></button>
-                        <button onClick={(e)=>{korinmas(e), setTelegram(true)}}><img src={img4} alt="" /></button>
-                        <button onClick={(e)=>{korinmas(e), setFeyzbuk(true)}}><img src={img1} alt="" /></button>
-                        <button onClick={(e)=>{korinmas(e), setGithub(true)}}><img src={img3} alt="" /></button>
+                        <button onClick={(e)=>{korinmas(e), setDunyo1(true)}}><img src={img5} alt="" /></button>
+                        <button onClick={(e)=>{korinmas(e), setInstagram1(true)}}><img src={img2} alt="" /></button>
+                        <button onClick={(e)=>{korinmas(e), setTelegram1(true)}}><img src={img4} alt="" /></button>
+                        <button onClick={(e)=>{korinmas(e), setFeyzbuk1(true)}}><img src={img1} alt="" /></button>
+                        <button onClick={(e)=>{korinmas(e), setGithub1(true)}}><img src={img3} alt="" /></button>
                     </div>
-                    {dunyo&&<input onChange={(e) => { setDunyo(e.target.value) }} id='name3' type="text" placeholder="Link kiriting" className="input input-bordered w-full" />}
-                    {instagram&&<input onChange={(e) => { setInstagram(e.target.value) }} id='name3' type="text" placeholder="Instagram link" className="input input-bordered w-full" />}
-                    {telegram&&<input onChange={(e) => { setTelegram(e.target.value) }} id='name3' type="text" placeholder="Telegram link" className="input input-bordered w-full" />}
-                    {feyzbuk&&<input onChange={(e) => { setFeyzbuk(e.target.value) }} id='name3' type="text" placeholder="Facbook link" className="input input-bordered w-full" />}
-                    {github&&<input onChange={(e) => { setGithub(e.target.value) }} id='name3' type="text" placeholder="Git Hub link" className="input input-bordered w-full" />}
+                    {dunyo1&&<input value={dunyo} onChange={(e) => { setDunyo(e.target.value) }} id='name3' type="text" placeholder="Link kiriting" className="input input-bordered w-full" />}
+                    {instagram1&&<input value={instagram} onChange={(e) => { setInstagram(e.target.value) }} id='name3' type="text" placeholder="Instagram link" className="input input-bordered w-full" />}
+                    {telegram1&&<input value={telegram} onChange={(e) => { setTelegram(e.target.value) }} id='name3' type="text" placeholder="Telegram link" className="input input-bordered w-full" />}
+                    {feyzbuk1&&<input value={feyzbuk } onChange={(e) => { setFeyzbuk(e.target.value) }} id='name3' type="text" placeholder="Facbook link" className="input input-bordered w-full" />}
+                    {github1&&<input value={github} onChange={(e) => { setGithub(e.target.value) }} id='name3' type="text" placeholder="Git Hub link" className="input input-bordered w-full" />}
 
                     </div>
 
                     <div className='div-8'>
                         <div>
                             <p className='label'>Davlat *</p>
-                            <select onChange={(e) => { setDavlat(e.target.value) }} className="select select-bordered w-full ">
+                            <select value={davlat} onChange={(e) => { setDavlat(e.target.value) }} className="select select-bordered w-full ">
                                 <option disabled selected>Davlat</option>
                                 <option>Uz</option>
                                 <option>Rs</option>
@@ -111,7 +130,7 @@ function Login() {
                         </div>
                         <div>
                             <p className='label'>Shahar *</p>
-                            <select onChange={(e) => { setShahar(e.target.value) }} className="select select-bordered w-full ">
+                            <select value={shahar} onChange={(e) => { setShahar(e.target.value) }} className="select select-bordered w-full ">
                                 <option disabled selected>Shahar</option>
                                 <option>Fargona</option>
                                 <option>Toshkent</option>
@@ -120,11 +139,11 @@ function Login() {
                     </div>
                     <div className='div-6'>
                         <label className='label' htmlFor="1212">Yashash joyi *</label>
-                        <input onChange={(e) => { setYashashJoy(e.target.value) }} id='1212' type="text" placeholder="Joy" className="input input-bordered w-full " />
+                        <input value={yashashJoy} onChange={(e) => { setYashashJoy(e.target.value) }} id='1212' type="text" placeholder="Joy" className="input input-bordered w-full " />
                         <label className='label' htmlFor="name5">Hodimlar soni *</label>
-                        <input onChange={(e) => { setHodimlar(e.target.value) }} id='name5' type="text" placeholder="Hodimlar soni *" className="input input-bordered w-full " />
+                        <input value={hodimlar} onChange={(e) => { setHodimlar(e.target.value) }} id='name5' type="text" placeholder="Hodimlar soni *" className="input input-bordered w-full " />
                         <label className='label' htmlFor="name6">Izoh *</label>
-                        <textarea onChange={(e) => { setIzoh(e.target.value) }} className='textarea textarea-bordered' name="" id="name6" placeholder='Kompaniya haqida izoh qoldiring' cols="30" rows="10" ></textarea>
+                        <textarea value={izoh} onChange={(e) => { setIzoh(e.target.value) }} className='textarea textarea-bordered' name="" id="name6" placeholder='Kompaniya haqida izoh qoldiring' cols="30" rows="10" ></textarea>
                     </div>
                     <div className='div-9'>
                         <button>Ortga</button>
